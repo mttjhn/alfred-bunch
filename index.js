84a6d0500f3fc8142ef7393d1458faeeed6b0b08
mttjhn <matthew@mcjohnsons.com>
2019-05-21 14:11:56 -0600

    Initial commit for workflow

---
+++ index.js
@@ -0,0 +1,35 @@
+const alfy = require('alfy');
+const glob = require("glob");
+
+const bunchFolder = '~/MegaSync/Apps/Bunch';
+
+//Load the bunches and let the user pick
+glob(bunchFolder + "/**/*.bunch", function (er, files) {
+    var bunches = []
+
+    files.forEach(function (value, index) {
+        if (value.length > bunchFolder.length + 6) {
+            var bunchName = value.substr(bunchFolder.length + 1, value.length - bunchFolder.length - 7)
+            bunches.push(bunchName);
+        }
+    });
+
+    if (alfy.input) {
+        var resultList = alfy
+            .inputMatches(bunches)
+            .map(x => ({
+                title: x.toLowerCase(),
+                subtitle: 'Action to run this bunch.',
+                arg: 'x-bunch://open?bunch=' + x.toLowerCase()
+            }));
+        alfy.output(resultList);
+    } else {
+        var resultList = bunches.map(x => ({
+            title: x.toLowerCase(),
+            subtitle: 'Action to run this bunch.',
+            arg: 'x-bunch://open?bunch=' + x.toLowerCase()
+        }));
+        alfy.output(resultList);
+    }
+});
+
